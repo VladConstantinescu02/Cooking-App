d6b31c9545c4aa01e2880a7b75b64ab7153c3f22
vladconstantinescu02 <constantinescuvladvsc@gmail.com>
2024-10-31 15:11:43 +0200

    featture/navbar

--- msa_cooking_app_client/lib/features/test/providers/ingredient_provider.dart
+++ msa_cooking_app_client/lib/features/test/providers/ingredient_provider.dart
@@ -15,7 +15,7 @@ Future<List<Ingredient>> ingredient(Ref red) async {
   ioc.badCertificateCallback = (X509Certificate cert, String host, int port) => true;
   final http = IOClient(ioc);
 
-  final response = await http.get(Uri.https("192.168.1.142:5001", "/api/ingredients"));
+  final response = await http.get(Uri.https("192.168.0.181:5001", "/api/ingredients"));
   if (response.statusCode == 200) {
     final json = jsonDecode(response.body);
     return json.map<Ingredient>((json) => Ingredient.fromJson(json)).toList();
