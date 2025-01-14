2ef29c205dffa30dceb7dcf9e5be8a412b2f3b5a
vladconstantinescu02 <constantinescuvladvsc@gmail.com>
2025-01-14 02:52:34 +0200

    Partially completed langing page

--- msa_cooking_app_client/lib/features/challenge/screens/challenge_screen.dart
+++ msa_cooking_app_client/lib/features/challenge/screens/challenge_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import '../widgets/challenge_card.dart';
+import 'package:lottie/lottie.dart';
 
 class ChallengeScreen extends StatelessWidget {
   const ChallengeScreen({super.key});
@@ -17,6 +19,56 @@ class ChallengeScreen extends StatelessWidget {
           ),
         ),
         elevation: 0,
+      ),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10.0),
+            child: Lottie.asset(
+              'images/stir_animation.json',
+              width: 300,
+              height: 300,
+            ),
+          ),
+
+          Expanded(
+            child: ListView(
+              padding: const EdgeInsets.symmetric(vertical: 10),
+              children: <Widget>[
+                ChallengeCard(
+                  onTap: () {
+                    debugPrint('Go to page');
+                  },
+                  icon: const Icon(Icons.set_meal_rounded, color: Colors.white, size: 40),
+                  text: 'Lets build your fridge',
+                  cardColor: const Color(0xFF8FB1BE),
+                  textColor: Colors.white,
+                  iconColor: Colors.white,
+                ),
+                ChallengeCard(
+                  onTap: () {
+                    debugPrint('Go to page');
+                  },
+                  icon: const Icon(Icons.fastfood, color: Colors.black, size: 40),
+                  text: 'Create your meals',
+                  cardColor: const Color(0xFFEEE3AB),
+                  textColor: Colors.black,
+                  iconColor: Colors.black,
+                ),
+                ChallengeCard(
+                  onTap: () {
+                    debugPrint('Go to page');
+                  },
+                  icon: const Icon(Icons.account_circle, color: Colors.white, size: 40),
+                  text: 'Set up your profile',
+                  cardColor: const Color(0xFF561D25),
+                  textColor: Colors.white,
+                  iconColor: Colors.white,
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
